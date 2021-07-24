7d398970fb4299840958e6053fd6dee22315ac79
ivissoncostasousa <82718167+ivissoncostasousa@users.noreply.github.com>
2021-07-24 08:45:41 -0300

    Create Classe.py

---
+++ Classe.py
@@ -0,0 +1,23 @@
+class Assinantes:
+    def _init_(self, nome, rg, telefone, numero):
+        self.nome = nome
+        self.rg = rg
+        self.telefone = telefone
+        self.numero = numero
+    def setNome(self, nome):
+        self.nome = nome
+    def set.Rg(self, rg):
+        self.rg = rg
+    def setTelefone(self, telefone):
+        self.telefone = telefone
+    def setNumero(self, numero):
+        self.numero = numero
+        
+    def getNome(self):
+            return self.nome
+    def getRg(self):
+            return self.rg
+    def getTelefone(self):
+            return self.telefone
+    def getNumero(self):
+            return self.numero
